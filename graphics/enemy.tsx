d4ed52fd9535d7ee82f0ba9d6e0ac6f96077684c
mekha03 <mekha@ualberta.ca>
2023-04-01 20:13:16 -0600

    graphics again

---
+++ graphics/enemy.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="enemy" tilewidth="389" tileheight="369" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="389" height="369" source="enemy-PhotoRoom.png-PhotoRoom.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="enemy-PhotoRoom.png-PhotoRoom_1_128x128.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="spider.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="spider.png"/>
+ </tile>
+</tileset>
