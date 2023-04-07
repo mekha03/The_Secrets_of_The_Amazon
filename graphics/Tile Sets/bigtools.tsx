cbd820f1f1ecc0d64ea2f847762ab3b8b483e750
mekha03 <mekha@ualberta.ca>
2023-04-07 17:57:26 -0600

    level 0 work

--- graphics/Tile Sets/bigtools.tsx
+++ graphics/Tile Sets/bigtools.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="bigtools" tilewidth="64" tileheight="64" tilecount="1024" columns="32">
- <image source="../../../../Downloads/tools_upscaled.png" width="2048" height="2048"/>
+<tileset version="1.10" tiledversion="1.10.0" name="bigtools" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="../Misc/tools_50.png" width="256" height="256"/>
 </tileset>
