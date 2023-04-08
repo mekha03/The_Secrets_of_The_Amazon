a41fe9dad9d276bf0c722cc864283fe2b66007d4
mekha03 <mekha@ualberta.ca>
2023-04-07 18:33:01 -0600

    level 0 info

--- Level/code/game_data.py
+++ Level/code/game_data.py
@@ -1,3 +1,7 @@
 level_0 = {
-    'terrain': '../levels/Level_0/level0'
-}
+    'terrain': '../levels/Level_0/level0._terrain.csv',
+    'coins': '../levels/Level_0/level0._coins.csv',
+    'constraints': '../levels/Level_0/level0._constraints.csv',
+    'background': '../levels/Level_0/level0._background.csv',
+    'enemy': '../levels/Level_0/level0._enemy.csv',
+    'player': '../levels/Level_0/level0._player.csv'}
--- Level/code/main.py
+++ Level/code/main.py
@@ -2,11 +2,12 @@ import pygame
 import sys
 from settings import *
 from level import Level
+from game_data import level_0
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_data, screen)
+level = Level(level_0, screen)
 
 while True:
     for event in pygame.event.get():
