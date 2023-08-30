988b4d2c2ae45ae27ec3e373fe9965cd13cfe5df
mekha03 <mekha@ualberta.ca>
2023-08-29 20:39:42 -0600

    remaking the level

---
+++ Level/levels/Level Data/Platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Platform" tilewidth="64" tileheight="64" tilecount="81" columns="9">
+ <image source="../../../../../../Pictures/2022-02-07/Platform Tiles.png" width="576" height="576"/>
+</tileset>
