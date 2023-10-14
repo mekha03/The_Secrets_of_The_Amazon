b230fbd728b96274fb7ea43770ceccc44417c0b2
mekha03 <mekha@ualberta.ca>
2023-10-14 14:28:05 -0600

    Updating enemy movements and starting the player character

--- Level/code/level.py
+++ Level/code/level.py
@@ -13,6 +13,12 @@ class Level:
         self.display_surface = surface
         self.world_shift = -2
 
+        # player
+        player_layout = import_csv_layout(level_data['player'])
+        self.player = pygame.sprite.GroupSingle()
+        self.goal = pygame.sprite.GroupSingle()
+        self.player_setup(player_layout)
+
         # terrain setup
         Platform_layout = import_csv_layout(level_data['Platform'])
         self.Platform_sprites = self.create_tile_group(Platform_layout, 'Platform')
@@ -28,6 +34,10 @@ class Level:
         # enemy_worm
         Enemy_worm_layout = import_csv_layout(level_data['Enemy_worm'])
         self.Enemy_worm_sprites = self.create_tile_group(Enemy_worm_layout,'Enemy_worm')
+
+        # Constraint enemy_worm
+        Constraints_worm_layout = import_csv_layout(level_data['Constraints_worm'])
+        self.Constraints_worm_sprites = self.create_tile_group(Constraints_worm_layout,'Constraints_worm')
 
 
     def create_tile_group(self, layout, type):
@@ -59,10 +69,29 @@ class Level:
                     if type == 'Enemy_worm':
                         sprite = Enemy(tile_size,x,y)
 
+                    if type == 'Constraints_worm':
+                        sprite = Tile(tile_size,x,y)
 
                     sprite_group.add(sprite)
         return sprite_group
 
+    def player_setup(self,layout):
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if val == '253':
+                    print('player goes here')
+                if val == '239':
+                    level_end_surface = pygame.image.load('../graphics/Misc/Level_end.png').convert_alpha()
+                    sprite = StaticTile(tile_size,x,y,level_end_surface)
+                    self.goal.add(sprite)
+
+    def enemy_collision_reverse(self):
+        for enemy in self.Enemy_worm_sprites.sprites():
+            if pygame.sprite.spritecollide(enemy,self.Constraints_worm_sprites,False):
+                enemy.reverse()
+    
     def run(self):
         # platform
         self.Platform_sprites.update(self.world_shift)
@@ -72,11 +101,17 @@ class Level:
         self.Plants_sprites.draw(self.display_surface)
         self.Plants_sprites.update(self.world_shift)
 
+        # enemy_worm
+        self.Enemy_worm_sprites.update(self.world_shift)
+        self.Constraints_worm_sprites.update(self.world_shift)
+        self.enemy_collision_reverse()
+        self.Enemy_worm_sprites.draw(self.display_surface)
+        
         # coins
         self.Coins_sprites.update(self.world_shift)
         self.Coins_sprites.draw(self.display_surface)
 
-        # enemy_worm
-        self.Enemy_worm_sprites.update(self.world_shift)
-        self.Enemy_worm_sprites.draw(self.display_surface)
+        # player sprites
+        self.goal.update(self.world_shift)
+        self.goal.draw(self.display_surface)
 
